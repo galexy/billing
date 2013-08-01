86ec5059c07486356afe1166f03b3b919c981bb8
Galex Yen <galex@reasoning.net>
2013-07-31 17:42:31 -0700

    Add error logging to mongodb

--- server/model/index.js
+++ server/model/index.js
@@ -2,9 +2,22 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODBURL || 'mongodb://localhost/billing', {
+var mongourl = process.env.MONGODBURL || 'mongodb://localhost/billing';
+console.log('Database url: ', mongourl);
+mongoose.connect(mongourl, {
   db: {native_parser: true}
 });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.on('connecting', console.log.bind(console, 'connecting to database:'));
+db.on('connected', console.log.bind(console, 'connected to database:'));
+db.on('disconnecting', console.log.bind(console, 'disconnecting from database:'));
+db.on('disconnected', console.log.bind(console, 'disconnected from database:'));
+db.on('close', console.log.bind(console, 'connection to database closed:'));
+db.on('reconnected', console.log.bind(console, 'reconnected to database:'));
+db.on('error', console.log.bind(console, 'database error:'));
+db.once('open', console.log.bind(console, 'opened database:'));
 
 module.exports.Product      = require('./product');
 module.exports.User         = require('./user');
